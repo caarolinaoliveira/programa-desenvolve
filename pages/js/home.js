743dc0b0839dd1f93162eba4d716c8383bfcc51b
caarolinaoliveira <138406827+caarolinaoliveira@users.noreply.github.com>
2024-08-05 15:26:37 +0000

    feat: lista de produtos em destaque criado

--- pages/js/home.js
+++ pages/js/home.js
@@ -11,3 +11,13 @@ function fecharMenuHamburguer() {
   var sidebar = document.getElementById("sidebar");
   sidebar.style.visibility = "hidden";
 }
+function adicionarFavorito(icon) {
+  // Verifica se a imagem é o ícone vermelho
+  if (icon.src.includes("icone-favorito-vermelho.png")) {
+    // Se for vermelho, volta para o ícone padrão
+    icon.src = "assets/icone-favorito.png";
+  } else {
+    // Se não for vermelho, muda para o ícone vermelho
+    icon.src = "assets/icone-favorito-vermelho.png";
+  }
+}
